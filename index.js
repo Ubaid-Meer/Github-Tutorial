798753b5fdd3b32ecd7d08c27be035fea6601038
Ubaid Ullah <ubaid13733@gmail.com>
2025-07-14 15:55:15 +0500

    University added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const name="ubaid";
 const age=22;
 const semester=7;
+const University="Iqra National University Peshawar";
+
 
 console.log("My name is " +  name +", i am "+age+ " year old "+"and  i persuing " + semester + " Semester");
+console.log(University);
+
 
