081e6a786466643e47157e0b626b2a4cbf311e64
Ubaid Ullah <ubaid13733@gmail.com>
2025-07-15 14:58:56 +0500

    the Multiplication Function has been added

--- test.js
+++ test.js
@@ -6,8 +6,14 @@ function test2(a,b){
     return a - b;
 
 }
+function test3(a,b){
+    return a * b;
+    
+}
 
 const add=test(34,6);
 const sub=test2(40,21);
+const multi=test3(10,300);
 
 console.log("Addition " + add,"Substraction "+ sub);
+console.log("Multiplication "+ multi);
